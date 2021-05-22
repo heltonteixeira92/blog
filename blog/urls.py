8d7fef383309ab5ef6a6b7ec83dbd48245b1a25a
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2021-05-22 18:29:51 -0300

    Adicionando padrões de urls as suas views

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+ from django.urls import path
+ from . import views
+
+ # criar um arquivo urls.py para cada app é a melhor maneira de tornar a app reutilizavel para outros proj.
+ app_name = 'blog' # isso permite organizar os urls por aplicação e usar o nome ao referenciá-los
+  urlpatterns = [
+    # views de postagens
+    path('', views.post_list, name='post_list'), # padro de url sem args e é mapeado para a view post_list
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+        views.post_detail,
+        name='post_detail'), # esse padrão recebe os arg year,month... e é mapeado para a view post_detail
+
+  ]
--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,10 @@ from .models import Post
 
 def post_list(request):  # a view post_list tem request como unico paramentro que é necessário em toda view
     posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})  # (objeto request, path do temple, variáveis de contexto)
-    # usamos o atalho render para renderizar a lista de postagens com o template espeicificado.
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts})  # (objeto request, path do temple, variáveis de contexto)
+    # usamos o atalho render para renderizar a lista de postagens com o template especificado.
 
 
 def post_detail(request, year, month, day, post):
