41c3b55ff582ec788cf3c79dc2e95044cd267dc4
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2021-05-26 18:11:48 -0300

    conhecendo class based view

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,8 @@ app_name = 'blog'  # isso permite organizar os urls por aplicação e usar o nom
 
 urlpatterns = [
     # views de postagens
-    path('', views.post_list, name='post_list'),  # padro de url sem args e é mapeado para a view post_list
+    # path('', views.post_list, name='post_list'),  # padro de url sem args e é mapeado para a view post_list
+    path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
     # esse padrão recebe os arg year,month... e é mapeado para a view post_detail
 
