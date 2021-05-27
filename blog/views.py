ccb31c12a384982dd42bb5b3c07d6c9b458dbad8
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2021-05-27 16:14:22 -0300

    renderizando formulario em template

--- blog/views.py
+++ blog/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from forms import EmailPostForm
+from blog.forms import EmailPostForm
 from django.core.mail import send_mail
 
 
 def post_list(request):  # a view post_list tem request como unico paramentro que é necessário em toda view
     object_list = Post.published.all()
-    paginator = Paginator(object_list, 3) # três postagens em cada página
+    paginator = Paginator(object_list, 3)  # três postagens em cada página
     page = request.GET.get('page')
     try:
         posts = paginator.page(page)
@@ -21,7 +21,7 @@ def post_list(request):  # a view post_list tem request como unico paramentro qu
         posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
-                  {'page': page,'posts': posts})  # (objeto request, path do temple, variáveis de contexto)
+                  {'page': page, 'posts': posts})  # (objeto request, path do temple, variáveis de contexto)
     # usamos o atalho render para renderizar a lista de postagens com o template especificado.
 
 
@@ -44,27 +44,25 @@ class PostListView(ListView):
 
 
 def post_share(request, post_id):
-    # obtém a postagem com base no id
+    # Obtém a postagem com base no id
     post = get_object_or_404(Post, id=post_id, status='published')
     sent = False
 
     if request.method == 'POST':
-        # Formulario foi submetido
+        # Formulário foi submetido
         form = EmailPostForm(request.POST)
         if form.is_valid():
-            # Campos de formulário passaram pela validação
+            # FCampos do formulário passaram pela validação
             cd = form.cleaned_data
-            post_url = request.build_absolute_uri(
-                post.get_absolute_url())
-            subject = f"{cd['name']} recommends you read " \
-                      f"{post.title}"
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
             message = f"Read {post.title} at {post_url}\n\n" \
                       f"{cd['name']}\'s comments: {cd['comments']}"
-            send_mail(subject, message, 'admin@myblog.com',
-                      [cd['to']])
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
             sent = True
-        else:
-            form = EmailPostForm()
-        return render(request, 'blog/post/share.html', {'post': post,
-                                                        'form': form,
-                                                        'sent': sent})
+
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
