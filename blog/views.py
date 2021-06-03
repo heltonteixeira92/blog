b3f32645a2034ce0a4de2fb73305068b4b8edec7
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2021-06-03 13:55:29 -0300

    dependencys with requirements.txt

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post, Comment
+from .models import Post, Comment # noqa
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from blog.forms import EmailPostForm, CommentForm
@@ -60,9 +60,9 @@ def post_detail(request, year, month, day, post):
     # Listas de postagens semelhantes
     post_tags_ids = post.tags.values_list('id', flat=True)
     similar_posts = Post.published.filter(tags__in=post_tags_ids)\
-                                    .exclude(id=post.id)
+                                  .exclude(id=post.id)
     similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
-                                .order_by('-same_tags', '-publish')[:4]
+                                 .order_by('-same_tags', '-publish')[:4]
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
