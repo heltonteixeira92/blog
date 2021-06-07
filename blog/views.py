0cd9c884d245da0bb2af3458d376d88591f49aa6
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2021-06-07 17:16:46 -0300

    classificação dos resultados

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from blog.forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 
 def post_list(request, tag_slug=None):  # a view post_list tem request como unico paramentro que é necessário em toda view
@@ -113,9 +113,12 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title','body')
+            search_query = SearchQuery(query)
             results = Post.published.annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(search=query)
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query),
+            ).filter(search=search_query).order_by('-rank')
     return render(request,
                   'blog/post/search.html',
                   {'form': form,
